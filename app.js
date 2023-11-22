5c6082b46cd83f5dd8c16f2c90b7f3fd5b070d1c
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-22 15:19:05 +0900

    chore: 세션 설정 수정

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const errorHandler = require("./middlewares/errorHandler");
 
@@ -12,14 +13,16 @@ mongoose
 	.then(() => console.log("Successfully connected to MongoDB"))
 	.catch((error) => console.error("Connection error", error));
 
+app.use(express.json());
 app.use(
 	session({
+		name: "exchangers.sid",
 		secret: process.env.SESSION_SECRET,
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
 			secure: false, // http도 허용
-			maxAge: 1000 * 60 * 60 * 24 * 7,
+			maxAge: 1000 * 60 * 60 * 24 * 3, // 3일
 		},
 	})
 );
