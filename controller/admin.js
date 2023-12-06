9a1da72e84ffa3a1ead0aa3df24fc1588edc8588
strongcookdas <goodjav141592@gmail.com>
2023-12-06 14:14:35 +0900

    chore : 어드민 api 중 장소 추가 api 삭제 - 일반 계정에서 할 수 있기 때문에 삭제

--- controller/admin.js
+++ controller/admin.js
@@ -2,32 +2,6 @@ const Location = require("../models/Location");
 const AddLocationRequest = require("../dto/admin/AddLocationRequest");
 const UpdateLocationRequest = require("../dto/admin/UpdateLocationRequest")
 
-exports.createLocation = async (req, res, next) => {
-    try {
-        const location = new AddLocationRequest(req.body);
-        const locationDoc = await Location.create(location);
-        await locationDoc.save();
-        res.json(location);
-    } catch (err) {
-        next(err);
-    }
-}
-
-exports.updateLocation = async (req, res, next) => {
-    try {
-        const updateRequest = new UpdateLocationRequest(req.body);
-        const updatedLocation = await Location.findByIdAndUpdate(req.params.id,updateRequest);
-        if(!updatedLocation){
-            const error = new Error("not found location");
-            error.status = 404;
-            return next(error)
-        }
-        res.json({result:"update location"});
-    } catch (error) {
-        next(error);
-    }
-}
-
 exports.deleteLocation = async (req, res, next) => {
     try {
         const deletedLocation = await Location.findById(req.params.id);
