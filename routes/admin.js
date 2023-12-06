52bb31e59accef1d909625790699688640224ccb
strongcookdas <goodjav141592@gmail.com>
2023-12-06 14:16:44 +0900

    fix : 파일 이동 및 이름 수정

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {acceptAddLocation, rejectAddLocation, deleteLocation, getNotAcceptedLocations} = require("../controller/admin");
+const {acceptAddLocation, rejectAddLocation, deleteLocation, getNotAcceptedLocations} = require("../controllers/adminController");
 
 router.delete("/locations/:id",deleteLocation);
 router.put("/locations-add/:id",acceptAddLocation);
