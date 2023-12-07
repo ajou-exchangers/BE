70d9963e32ac815c6ee9a947a07c07042d324ef7
Hyeonji-Jung <welloff.jj@gmail.com>
2023-12-07 16:00:50 +0900

    chore: board, comment 에러 메시지 추가

--- constants/errorMessage.js
+++ constants/errorMessage.js
@@ -5,6 +5,11 @@ const RESPONSE_MESSAGE = {
 	USER_REVIEW_NOT_FOUND:
 		"This review was not user-written or the review does not exist.",
 	ADMIN_LOGIN_FAIL: "This is not an admin account.",
+
+	POST_NOT_FOUND: "Post not found",
+	COMMENT_NOT_FOUND: "Comment not found",
+	NOT_THE_AUTHOR: "Not the author",
+	COMMENT_NOT_IN_THE_POST: "Comment not in the post",
 };
 
 module.exports = RESPONSE_MESSAGE;
