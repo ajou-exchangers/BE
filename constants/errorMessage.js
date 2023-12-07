4a90c4c10b95cab9a8d6f0a6ff3b72c421510b8b
Hyeonji-Jung <welloff.jj@gmail.com>
2023-12-07 15:50:00 +0900

    fix: error message 사소한 오타 수정

--- constants/errorMessage.js
+++ constants/errorMessage.js
@@ -1,9 +1,10 @@
 const RESPONSE_MESSAGE = {
-    LOCATION_NOT_FOUND: "Location not found",
-    REVIEW_NOT_FOUND: "Review not found",
-    FORBIDDEN_MESSAGE: "You do not have permission.",
-    USER_RIVEW_NOT_FOUND: "This review was not user-written or the review does not exist.",
-    ADMIN_LOGIN_FAIL: "This is not an admin account.",
+	LOCATION_NOT_FOUND: "Location not found",
+	REVIEW_NOT_FOUND: "Review not found",
+	FORBIDDEN_MESSAGE: "You do not have permission.",
+	USER_REVIEW_NOT_FOUND:
+		"This review was not user-written or the review does not exist.",
+	ADMIN_LOGIN_FAIL: "This is not an admin account.",
 };
 
 module.exports = RESPONSE_MESSAGE;
