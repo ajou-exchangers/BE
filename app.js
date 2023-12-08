84561dbf48b3b9b86c7ea5521c4639353215da4d
strongcookdas <goodjav141592@gmail.com>
2023-12-08 18:15:22 +0900

    chore : cookie httpOnly 옵션 주석 처리

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
 		saveUninitialized: false,
 		proxy: true,
 		cookie: {
-			httpOnly: true,
+			// httpOnly: true,
 			secure: true,
 			sameSite: "none",
 			maxAge: 1000 * 60 * 60 * 24 * 3, // 3일
