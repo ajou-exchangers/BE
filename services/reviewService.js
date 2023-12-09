7a958c954f01a79e425a78796683e1ccf8fee743
strongcookdas <goodjav141592@gmail.com>
2023-12-10 00:13:04 +0900

    chore : review crud시 트랜잭션 적용 - location의 review관련 캐시컬럼도 업데이트하기 때문에 데이터 일관성을 위해 트랜잭션 적용

--- services/reviewService.js
+++ services/reviewService.js
@@ -3,29 +3,49 @@ const Review = require("../models/Review");
 const CustomError = require("../utils/CustomError");
 const ERROR_CODES = require("../constants/errorCodes");
 const ERROR_MESSAGE = require("../constants/errorMessage");
+const mongoose = require("mongoose");
 
 exports.writeReview = async (writeReviewRequest, locationId, userId) => {
-    const location = await Location.findById(locationId);
-    if (!location || !location.isVisible) {
-        throw CustomError(
-            ERROR_CODES.NOT_FOUND,
-            ERROR_MESSAGE.LOCATION_NOT_FOUND
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const location = await Location.findById(locationId).session(session);
+        if (!location || !location.isVisible) {
+            throw CustomError(
+                ERROR_CODES.NOT_FOUND,
+                ERROR_MESSAGE.LOCATION_NOT_FOUND
+            );
+        }
+
+        await Review.create(
+            [
+                {
+                    location: locationId,
+                    user: userId,
+                    ...writeReviewRequest,
+                },
+            ],
+            { session }
         );
-    }
 
-    const review = await Review.create({
-        location: locationId,
-        user: userId,
-        ...writeReviewRequest,
-    });
-    await review.save();
+        console.log(typeof writeReviewRequest.rating);
+        location.reviewTotalGrade += parseFloat(writeReviewRequest.rating);
+        location.reviewCount += 1;
+        if (location.reviewCount !== 0) {
+            location.reviewAverage = (
+                location.reviewTotalGrade / location.reviewCount
+            ).toFixed(1);
+        }
 
-    location.reviewTotalGrade += review.rating;
-    location.reviewCount += 1;
-    if (location.reviewCount !== 0) {
-        location.reviewAverage = (location.reviewTotalGrade / location.reviewCount).toFixed(1);
+        await location.save();
+        await session.commitTransaction();
+        session.endSession();
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
     }
-    await location.save();
 };
 
 exports.getReviews = async () => {
@@ -57,59 +77,90 @@ exports.getReviewsByLocation = async (locationId) => {
 };
 
 exports.updateReview = async (updateReviewRequest, reviewId, userId) => {
-    const review = await Review.findById(reviewId);
-    if (!review) {
-        throw CustomError(
-            ERROR_CODES.NOT_FOUND,
-            ERROR_MESSAGE.USER_REVIEW_NOT_FOUND
-        );
-    }
-    if (review.user.toString() !== userId) {
-        throw CustomError(
-            ERROR_CODES.FORBIDDEN,
-            ERROR_MESSAGE.FORBIDDEN_MESSAGE
-        );
-    }
-    const location = await Location.findById(review.location);
-    const reviewRating = review.rating;
-    Object.assign(review, {...updateReviewRequest});
-    await review.save();
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
-    if (location) {
-        location.reviewTotalGrade -= reviewRating;
-        location.reviewTotalGrade += review.rating;
-        if (location.reviewCount !== 0) {
-            location.reviewAverage = (location.reviewTotalGrade / location.reviewCount).toFixed(1);
+    try {
+        const review = await Review.findById(reviewId).session(session);
+        if (!review) {
+            throw CustomError(
+                ERROR_CODES.NOT_FOUND,
+                ERROR_MESSAGE.USER_REVIEW_NOT_FOUND
+            );
         }
-        await location.save();
+        if (review.user.toString() !== userId) {
+            throw CustomError(
+                ERROR_CODES.FORBIDDEN,
+                ERROR_MESSAGE.FORBIDDEN_MESSAGE
+            );
+        }
+
+        const location = await Location.findById(review.location).session(session);
+        const reviewRating = review.rating;
+
+        Object.assign(review, { ...updateReviewRequest });
+        await review.save();
+
+        if (location) {
+            location.reviewTotalGrade -= reviewRating;
+            location.reviewTotalGrade += review.rating;
+
+            if (location.reviewCount !== 0) {
+                location.reviewAverage = (location.reviewTotalGrade / location.reviewCount).toFixed(1);
+            }
+
+            await location.save();
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
     }
 };
 
 exports.deleteReview = async (reviewId, userId) => {
-    const review = await Review.findById(reviewId);
-    if (!review) {
-        throw CustomError(
-            ERROR_CODES.NOT_FOUND,
-            ERROR_MESSAGE.REVIEW_NOT_FOUND
-        );
-    }
-    if (review.user.toString() !== userId) {
-        throw CustomError(
-            ERROR_CODES.FORBIDDEN,
-            ERROR_MESSAGE.FORBIDDEN_MESSAGE
-        );
-    }
-    const location = await Location.findById(review.location);
-    const reviewRating = review.rating;
-    await Review.deleteOne(review);
-    if (location) {
-        location.reviewTotalGrade -= reviewRating;
-        location.reviewCount -= 1;
-        if (location.reviewCount !== 0) {
-            location.reviewAverage = (location.reviewTotalGrade / location.reviewCount).toFixed(1);
-        } else {
-            location.reviewAverage = 0;
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const review = await Review.findById(reviewId).session(session);
+        if (!review) {
+            throw CustomError(
+                ERROR_CODES.NOT_FOUND,
+                ERROR_MESSAGE.REVIEW_NOT_FOUND
+            );
         }
-        await location.save();
+        if (review.user.toString() !== userId) {
+            throw CustomError(
+                ERROR_CODES.FORBIDDEN,
+                ERROR_MESSAGE.FORBIDDEN_MESSAGE
+            );
+        }
+
+        const location = await Location.findById(review.location).session(session);
+        const reviewRating = review.rating;
+
+        await Review.deleteOne(review);
+
+        if (location) {
+            location.reviewTotalGrade -= reviewRating;
+            location.reviewCount -= 1;
+            if (location.reviewCount !== 0) {
+                location.reviewAverage = (location.reviewTotalGrade / location.reviewCount).toFixed(1);
+            } else {
+                location.reviewAverage = 0;
+            }
+            await location.save();
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
     }
 };
