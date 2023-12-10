0129d8addbf545e510b2adde8e1f303f94c20330
strongcookdas <goodjav141592@gmail.com>
2023-12-10 15:03:29 +0900

    fix : 리뷰 작성 request dto 수정

--- dto/review/ReviewRequest.js
+++ dto/review/ReviewRequest.js
@@ -1,10 +1,10 @@
 class WriteReviewRequest {
     constructor({rating, keywords, review, images}) {
         this.rating = rating;
-        this.keywords = keywords;
+        this.keywords = Array.isArray(keywords) ? keywords : JSON.parse(keywords);
         this.review = review;
         this.images = images;
-    }
+    };
 }
 
 module.exports = WriteReviewRequest;
